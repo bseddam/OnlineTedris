bfc6f629d10307675ac80dc812c1d8cb1bf2f858
seddam <bseddam@gmail.com>
2019-01-30 17:01:45 +0300

    derslik novu

--- OnlineTedrisGit/Models/DbModels/Dersliknovu.cs
+++ OnlineTedrisGit/Models/DbModels/Dersliknovu.cs
@@ -8,7 +8,7 @@ namespace OnlineTedris.Models.DbModels
     public class Dersliknovu
     {
         public int ID{ get; set; }
-        public int DerslikAdi { get; set; }
+        public string DerslikAdi { get; set; }
         public List<TedrisMovzulari> TedrisMovzularis { get; set; }
     }
 }
